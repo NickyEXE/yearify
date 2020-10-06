38e6c1b1572013f6102a416a863069ab0ca9d923
NickyEXE <dovern42@gmail.com>
2020-10-06 19:43:33 -0400

    debugs offset issue, is alive and complete

--- app/models/spotify_source_playlist.rb
+++ app/models/spotify_source_playlist.rb
@@ -11,7 +11,7 @@ class SpotifySourcePlaylist < ApplicationRecord
   end
 
   def fetch_songs(token, offset)
-    res = JSON.parse(`curl -H "Authorization: Bearer #{token}" https://api.spotify.com/v1/playlists/#{spotify_id}/tracks?limit=100&offset=#{offset}`)
+    res = JSON.parse(`curl -H "Authorization: Bearer #{token}" https://api.spotify.com/v1/playlists/#{spotify_id}/tracks?offset=#{offset}`)
     save_songs(res)
     return res["total"]
   end
@@ -19,15 +19,18 @@ class SpotifySourcePlaylist < ApplicationRecord
   def save_songs(res)
     res["items"].each do |item|
       track = item["track"]
-      Song.find_or_create_by(spotify_id: track["id"], user: user) do |s|
-        if track["album"]["release_date"]
-          date_array = track["album"]["release_date"].split("-")
-          s.release_date = DateTime.new(date_array[0].to_i)
+      unless !track
+        song = Song.find_or_create_by(spotify_id: track["id"], user: user) do |s|
+          if track["album"]["release_date"]
+            date_array = track["album"]["release_date"].split("-")
+            s.release_date = DateTime.new(date_array[0].to_i)
+          end
+          s.artist = track["artists"][0]["name"]
+          s.album = track["album"]["name"]
+          s.uri = track["uri"]
+          s.user = user
+          s.spotify_source_playlist = self
         end
-        s.artist = track["artists"][0]["name"]
-        s.album = track["album"]["name"]
-        s.uri = track["uri"]
-        s.user = user
       end
     end
   end
@@ -37,16 +40,18 @@ class SpotifySourcePlaylist < ApplicationRecord
     first_playlists = get_by_token_and_offset(token, 0)
     create_playlists(first_playlists, user)
     total_playlists = first_playlists["total"]
-    i = 50
+    i = 20
     while i < total_playlists do
       playlists = get_by_token_and_offset(token, i)
       create_playlists(playlists, user)
-      i = i + 50
+      i = i + 20
     end
   end
 
   def self.get_by_token_and_offset(token, offset)
-    return JSON.parse(`curl -H "Authorization: Bearer #{token}" https://api.spotify.com/v1/me/playlists?limit=50&offset=#{offset}`)
+    route = "https://api.spotify.com/v1/me/playlists?offset=#{offset}"
+    response = JSON.parse(`curl -H "Authorization: Bearer #{token}" #{route}`)
+    return response
   end
 
   def self.create_playlists(playlists, user)
