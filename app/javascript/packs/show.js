58919d9aa2b875b866a4f5101da94941b544a346
NickyEXE <dovern42@gmail.com>
2020-10-06 20:25:59 -0400

    hides link with JS

---
+++ app/javascript/packs/show.js
@@ -0,0 +1,3 @@
+document.getElementById('generate').addEventListener("click", () => {
+  document.querySelector("ul").outerHTML = `<h3>Your playlists are being generated. Please check back in an hour or so.</h3>`
+})
