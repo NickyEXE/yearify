6f67ecd163ea1edea7ac43b39130e0438e5d4803
NickyEXE <dovern42@gmail.com>
2021-08-15 18:27:08 -0400

    add scope method, add get_self method

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,12 @@ class Song < ApplicationRecord
   belongs_to :spotify_source_playlist
   belongs_to :user
 
+  scope :not_compilation, -> {where.not(album_type: "compilation").or(Song.where(album_type: nil))}
+
+  def get_self
+    SpotifyApi.get(user, "/tracks/#{spotify_id}")
+  end
+
   def self.by_year(songs)
     songs.group_by{|s| s.release_date ? s.release_date.year : "not_sorted"}
   end
