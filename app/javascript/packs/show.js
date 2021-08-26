8f8846fb2d81e07e7705707d50ec5d5f7a0b2a84
NickyEXE <dovern42@gmail.com>
2021-08-25 22:38:51 -0400

    destroy all js

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,3 +1,7 @@
 document.getElementById('generate').addEventListener("click", () => {
-  document.querySelector("ul").outerHTML = `<h3>Your playlists are being generated. Please check back in an hour or so.</h3>`
+  document.querySelector("ul").outerHTML = `<h3>Your playlists are being generated. Please check back in an hour or so. Feel free to leave the page.</h3>`
+})
+
+document.getElementById('destroy-all').addEventListener("click", () => {
+  document.querySelector("ul").outerHTML = `<h3>Your playlists are being destroyed. This may take a little bit. Feel free to leave the page.</h3>`
 })
