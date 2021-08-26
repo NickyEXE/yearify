85603241dfc862478a10bc5073a125bb665e7b99
NickyEXE <dovern42@gmail.com>
2021-08-26 02:12:05 -0400

    make link trigger alert

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,5 +1,5 @@
 document.getElementById('generate').addEventListener("click", () => {
-  document.querySelector("ul").outerHTML = `<h3>Your playlists are being generated. Please check back in an hour or so. Feel free to leave the page.</h3>`
+  alert(`<h3>Your playlists are being generated. Please check back in an hour or so. Feel free to leave the page.</h3>`)
 })
 
 document.getElementById('destroy-all').addEventListener("click", () => {
