869fa6e060f611ad76d75c7facd61ed85107b62f
NickyEXE <dovern42@gmail.com>
2021-08-27 00:06:10 -0400

    update needed requests logic

--- app/workers/source_playlist/songs_worker.rb
+++ app/workers/source_playlist/songs_worker.rb
@@ -5,7 +5,10 @@ class SourcePlaylist::SongsWorker
     playlist = SpotifySourcePlaylist.find(playlist_id)
     total = playlist.fetch_songs(token, offset)
     if !playlist.needed_requests
-      playlist.update(needed_requests: total/100 + 1, total_requests: playlist.total_requests + 1)
+      playlist.update(needed_requests: total/100 + 1, total_requests: 1)
+    else
+      playlist.update(total_requests: playlist.total_requests + 1)
+    end
     if offset < total
       SourcePlaylist::SongsWorker.perform_async(playlist_id, token, offset + 100)
     end
