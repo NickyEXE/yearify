b275ee42557af458709f8e531ca4d5e39f7d0d59
NickyEXE <dovern42@gmail.com>
2021-08-27 00:05:50 -0400

    fix updated method name

--- app/workers/source_playlist/import_worker.rb
+++ app/workers/source_playlist/import_worker.rb
@@ -3,6 +3,6 @@ class SourcePlaylist::ImportWorker
 
   def perform(token, offset, user_id)
     playlists = SpotifySourcePlaylist.get_by_token_and_offset(token, offset)
-    SpotifySourcePlaylist.create_playlists_with_id(playlists, user_id)
+    SpotifySourcePlaylist.create_playlists(playlists, user_id)
   end
 end
